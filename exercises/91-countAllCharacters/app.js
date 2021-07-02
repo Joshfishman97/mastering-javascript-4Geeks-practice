dc620e367163634d5af557849013b813a984b1e3
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-02 21:44:23 +0000

    saving

--- exercises/91-countAllCharacters/app.js
+++ exercises/91-countAllCharacters/app.js
@@ -1,19 +1,19 @@
 function countAllCharacters(para1) {
+    let answer = {};
     let string = para1.split("")
     for (let index = 0; index < string.length; index++) {
         const Letter = string[index]
         if (answer[Letter] === undefined) {
             answer[Letter] = 1
         }
-        else  answer[Letter.toLowerCase()] = answer[Letter.toLowerCase()] + 1;
+        else answer[Letter.toLowerCase()] = answer[Letter.toLowerCase()] + 1;{}
 
     }
-return answer;
+    return answer;
 }
 
 
 
 
-let answer = {};
 let output = countAllCharacters('banana');
 console.log(output)
--- exercises/92-getElementsGreaterThanTenAtProperty/app.js
+++ exercises/92-getElementsGreaterThanTenAtProperty/app.js
@@ -1,1 +1,31 @@
-// Write your function here
+function getElementsGreaterThan10AtProperty(obj, key) {
+    let newArray = obj[key];
+    let answer = [];
+    for (let elements in obj[key]) {
+        if (newArray.length === []) {
+            return [];
+        }
+        if (newArray[elements].length < 10) {
+          return [];
+        }
+        if (obj[key][elements] ==! []) {
+            return [];
+        }
+        if (obj[key] === undefined) {
+            return [];
+
+        }
+        else if(newArray[elements]>10){
+            answer.push(newArray[elements])
+        }
+    }
+    return answer;
+}
+
+
+
+    var obj = {
+        key: [1, 20, 30]
+    };
+    var output = getElementsGreaterThan10AtProperty(obj, 'key');
+    console.log(output);
