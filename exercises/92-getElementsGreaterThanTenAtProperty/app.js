d060e53459f5fb07ba21696d7246ac333c7a38f7
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-06 22:51:27 +0000

    a

--- exercises/92-getElementsGreaterThanTenAtProperty/app.js
+++ exercises/92-getElementsGreaterThanTenAtProperty/app.js
@@ -1,28 +1,15 @@
 function getElementsGreaterThan10AtProperty(obj, key) {
     let newArray = obj[key];
     let answer = [];
-    for (let elements in obj[key]) {
-        if (newArray.length === []) {
-            return [];
-        }
-        if (newArray[elements].length < 10) {
-          return [];
-        }
-        if (obj[key][elements] ==! []) {
-            return [];
-        }
-        if (obj[key] === undefined) {
-            return [];
-
-        }
-        else if(newArray[elements]>10){
-            answer.push(newArray[elements])
-        }
+   if(typeof newArray === "object" || newArray !== undefined){
+       for(let i= 0; i<newArray.length; i++){
+           if(newArray[i]>10){
+               answer.push(newArray[i])
+           }
+       }
+   }
+   return answer
     }
-    return answer;
-}
-
-
 
     var obj = {
         key: [1, 20, 30]
