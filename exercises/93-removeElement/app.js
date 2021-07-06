99809a4594b73ac9efb42b02f6f878abb9036024
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-06 23:32:35 +0000

    a

--- exercises/93-removeElement/app.js
+++ exercises/93-removeElement/app.js
@@ -1,1 +1,9 @@
-// Write your function here
+function removeElement(arr,discarder){
+    let ArrWithDiscarder = arr.filter((item,index)=>item!=discarder)
+    return ArrWithDiscarder
+}
+
+
+
+var output = removeElement([1, 2, 3, 2, 1], 2); 
+console.log(output); // --> [1, 3, 1]
