882680e7976c9c4cca99997ff9554af04e1b4407
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-08 21:33:09 +0000

    another commit

--- exercises/94-getFirstElementOfProperty/app.js
+++ exercises/94-getFirstElementOfProperty/app.js
@@ -1,1 +1,18 @@
 // Write your function here
+
+function getFirstElementOfProperty(obj,key){
+    var newArray = obj[key]
+    if(typeof newArray === 'object'|| newArray != undefined){
+        for(let i = 0; i<newArray.length; i++){
+            
+        }
+        return newArray[0]
+    }
+
+}
+
+var obj = {
+    key: [1, 2, 4]
+  };
+  var output = getFirstElementOfProperty(obj, 'key');
+  console.log(output); 
