5c83cc19611524f1a25c1e94ae0c68f3d155fe7b
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-08 21:46:10 +0000

    commit do 95 and 96 next

--- exercises/95-getNthElementOfProperty/app.js
+++ exercises/95-getNthElementOfProperty/app.js
@@ -1,1 +1,14 @@
-// Write your function here
+function getNthElementOfProperty(obj,key,n){
+    let newArray = obj[key]
+    if(n)
+    for(let i = 0; i<newArray.length; i++){
+
+    }
+}
+
+
+var obj = {
+    key: [1, 2, 6]
+  };
+  var output = getNthElementOfProperty(obj, 'key', 1);
+  console.log(output); // 
--- exercises/96-getLastElementOfProperty/app.js
+++ exercises/96-getLastElementOfProperty/app.js
@@ -1,1 +1,16 @@
-// Write your function here
+function getLastElementOfProperty(obj,key){
+    let newArray = obj[key]
+    let answer = [];
+    if(typeof newArray === 'object' || newArray!=[]){
+        for(let i = 0; i<newArray.length; i++){
+            return newArray[2]
+        }
+    }
+}
+
+
+var obj = {
+    key: [1, 2, 5]
+  };
+  var output = getLastElementOfProperty(obj, 'key');
+  console.log(output); 
